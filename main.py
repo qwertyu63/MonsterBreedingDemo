3e779979f2f54adacf0afd6137d14b72a52a9c80
Nicholas Nicholas <computermaster124816@gmail.com>
2021-01-06 01:40:37 +0000

    Replaced ID records with bloodline genes.

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 class monster(object):
-    def __init__(self, genes, parents, line, sex, name,age=0):
+    def __init__(self, genes, sex, name,age=0):
         self.color = None
         self.age = age
         self.genes = genes
         self.stats = [0,0,0,0]
         self.skills = [0,0,0,0]
-        self.parents = parents
-        self.line = line
-        self.id = randint(100000,999999)
         self.sex = sex
         self.name = name
         if genes[4][0]==genes[4][2]:
@@ -76,20 +73,19 @@ Speed: %i (%i%i%i)
 Brain: %i (%i%i%i)
 Charm: %i (%i%i%i)
 Color: %s (%s)
-        """%(self.name,icon,self.age,self.stats[0],self.genes[0][0],self.genes[0][1],self.genes[0][2],self.stats[1],self.genes[1][0],self.genes[1][1],self.genes[1][2],self.stats[2],self.genes[2][0],self.genes[2][1],self.genes[2][2],self.stats[3],self.genes[3][0],self.genes[3][1],self.genes[3][2],cicon,colorgene)
+Core Genes: %s-%s-%s-%s
+        """%(self.name,icon,self.age,self.stats[0],self.genes[0][0],self.genes[0][1],self.genes[0][2],self.stats[1],self.genes[1][0],self.genes[1][1],self.genes[1][2],self.stats[2],self.genes[2][0],self.genes[2][1],self.genes[2][2],self.stats[3],self.genes[3][0],self.genes[3][1],self.genes[3][2],cicon,colorgene,self.genes[5][0],self.genes[5][1],self.genes[5][2],self.genes[5][3])
     def savebuild(self):
         save = self.genes[0]
         save += self.genes[1]
         save += self.genes[2]
         save += self.genes[3]
         save += self.genes[4]
+        save += self.genes[5]
         save += self.skills
-        save += self.parents
-        save.append(self.id)
         save.append(self.age)
         save.append(self.sex)
         save.append(self.name)
-        save += self.line
         for i in range(0,len(save)):
             save[i]=str(save[i])
         save=tuple(save)
@@ -97,26 +93,24 @@ Color: %s (%s)
     def saveload(self, save):
         save = list(save)
         for i in range(0,len(save)):
-            if i != 24:
+            if i != 25:
                 save[i]=int(save[i])
         self.genes[0]=save[0:3]
         self.genes[1]=save[3:6]
         self.genes[2]=save[6:9]
         self.genes[3]=save[9:12]
         self.genes[4]=save[12:15]
-        self.skills=save[15:19]
-        self.parents=save[19:21]
-        self.id=save[21]
-        self.age=save[22]
-        self.sex=save[23]
-        self.name=save[24]
-        self.line=save[25:]
+        self.genes[4]=save[19:19]
+        self.skills=save[19:23]
+        self.age=save[23]
+        self.sex=save[24]
+        self.name=save[25]
+        self.genstats()
 
-from random import randint
+from random import randint, shuffle
 from time import time
-from math import floor, ceil
-import shelve
 from os.path import isfile
+import shelve
 
 def colorgenes(genes):
     letters=""
@@ -136,16 +130,20 @@ def sortmon(e):
 
 def breed(mom, dad, sex=None):
     print("\n%s and %s are trying to breed."%(mom.name,dad.name))
-    penalty, line = bloodcheck(mom,dad)
-    parents = [mom.id,dad.id]
+    penalty = bloodcheck(mom,dad)
     testpass = alert(mom,dad,penalty)
     if not testpass:
-        genes=[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+        genes=[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0,0]]
         for i in range(0,5):
             genes[i][0]=mom.genes[i][randint(0,2)]
             genes[i][1]=dad.genes[i][randint(0,2)]
             genes[i][2]=randint(1,4)
         genes[4][2]=genes[4][randint(0,1)]
+        genes[5][0]=mom.genes[5][0]
+        genes[5][1]=dad.genes[5][0]
+        genes[5][2]=mom.genes[5][randint(1,3)]
+        genes[5][3]=dad.genes[5][randint(1,3)]
+        shuffle(genes[5])
         while penalty != 0:
             genes[randint(0,3)][randint(0,2)]=0
             penalty -= 1
@@ -153,26 +151,22 @@ def breed(mom, dad, sex=None):
             sex = randint(0,1)
         newname = namegen()
         print("Their offspring is named %s."%(newname))
-        return monster(genes,parents,line,sex,newname)
+        return monster(genes,sex,newname)
     else:
         return None
 
 def alert(mom,dad,penalty):
     alert=False
     if mom.age==1 or mom.age==0:
-        pass
-        #print("%s is too young to have offspring."%(mom.name))
-        #alert=True
-        #disabled for testing
+        print("%s is too young to have offspring."%(mom.name))
+        alert=True
     if dad.age==1 or dad.age==0:
-        pass
-        #print("%s is too young to have offspring."%(dad.name))
-        #alert=True
-        #disabled for testing
+        print("%s is too young to have offspring."%(dad.name))
+        alert=True
     if mom.sex==dad.sex and mom.sex!=2:
         print("%s and %s are the same sex; they can't mate."%(mom.name,dad.name))
         alert=True
-    if penalty==2 and alert==False:
+    if penalty>1 and alert==False:
         print("%s and %s are closely related.\nTheir offspring will be much weaker than normal."%(mom.name,dad.name))
     if penalty==1 and alert==False:
         print("%s and %s are related.\nTheir offspring will be weaker than normal."%(mom.name,dad.name))
@@ -180,31 +174,10 @@ def alert(mom,dad,penalty):
 
 def bloodcheck(mom, dad):
     penalty = 0
-    line = mom.parents+dad.parents
-    line = list(set(line))
-    try:
-        line.remove(0)
-    except ValueError:
-        pass
-    line.sort()
-    for i in range(0,2):
-        if mom.parents[i] in dad.parents:
-            if mom.parents[0] != 0:
-                penalty = 2
-    if mom.id in dad.parents:
-        penalty=2
-    if dad.id in mom.parents:
-        penalty=2
-    if penalty==0:
-        oldline = mom.line+dad.line+line
-        cleanline = []
-        for i in range(0,len(oldline)):
-            if oldline[i] in cleanline:
-                penalty=1
-                break
-            else:
-                cleanline.append(oldline[i])
-    return (penalty, line)
+    for i in range(0,4):
+        if mom.genes[5][i] in dad.genes[5]:
+            penalty += 1
+    return penalty
 
 def displaybox(box):
     box.sort(key=sortmon)
@@ -229,13 +202,15 @@ def namegen():
     return name.capitalize()
 
 def randomgene():
-    genes=[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+    genes=[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0,0]]
     for i in range(0,4):
         for j in range(0,3):
             genes[i][j]=randint(1,3)
     genes[4][0]=randint(0,3)
     genes[4][1]=genes[4][0]
     genes[4][2]=genes[4][0]
+    for i in range(0,4):
+        genes[5][i]=randint(100,999)
     return genes
 
 def inspect(box):
@@ -255,6 +230,7 @@ def inspect(box):
                     box.append(lookup)
         else:
             print(lookup.fullprint())
+            box.append(lookup)
 
 def checkbays(box,bays):
     while True:
@@ -283,6 +259,7 @@ def checkbays(box,bays):
 
 def bayedit(bay,box,name="bay"):
     while True:
+        flag = False
         if name == "stage":
             phrase = "on the stage"
             label = "On Stage"
@@ -294,7 +271,7 @@ def bayedit(bay,box,name="bay"):
             if eject != None:
                 box.append(eject)
             else:
-                break
+                flag = True
         else:
             if len(bay) == 1:
                 print("\n"+label+": "+str(bay[0]))
@@ -306,6 +283,10 @@ def bayedit(bay,box,name="bay"):
                 box.append(bay.pop(0))
             elif output != None:
                 bay.append(output)
+            elif output == None:
+                flag = True
+        if name == "stage":
+            return flag
 
 def getmonster(box,message="Which monster do you want?\nInput 0 to cancel.",drop=False,rename=False):
     displaybox(box)
@@ -329,8 +310,7 @@ def getmonster(box,message="Which monster do you want?\nInput 0 to cancel.",drop
             print("There's not that many monsters to choose from.")
 
 def fullbay(bay,name="bay"):
-        print("\n"+name.capitalize(),end="")
-        print(bay)
+        print("\n"+name.capitalize()+":",end="")
         displaybox(bay)
         print("That %s is already full.\nInput 1 or 2 to remove that monster.\nAny other input cancels."%(name))
         remove = None
@@ -345,42 +325,10 @@ def fullbay(bay,name="bay"):
 
 def exhibit(box,stage,stagetarget):
     while True:
-        print("\nThis month's show is about %s.\n"%(statname[stagetarget]))
-        if len(stage) == 2:
-            print("On Stage:",end="")
-            displaybox(stage)
-            print("The stage is already full.\nInput 1 or 2 to remove that monster.\nAny other input cancels.")
-            target = input("> ")
-            if target == "1":
-                print("\n%s has been removed from the stage."%(stage[0].name))
-                box.append(stage.pop(0))
-            elif target == "2":
-                print("\n%s has been removed from the stage."%(stage[1].name))
-                box.append(stage.pop(1))
-            else:
-                return
-        else:
-            if len(stage) == 1:
-                print("\nOn Stage: "+str(stage[0]))
-            print("\nIn Box:",end="")
-            displaybox(box)
-            print("Which monster do you want to add to the stage?\nInput 0 to cancel.")
-            if len(stage)==1:
-                print("Input R to remove %s from the stage."%(stage[0].name))
-            target = input("> ")
-            if target in ("0",""):
-                return
-            elif (target == "R" or target == "r") and len(stage) == 1:
-                print("\n%s has been removed from the stage."%(stage[0].name))
-                box.append(stage.pop(0))
-            else:
-                try:
-                    stage.append(box.pop(int(target)-1))
-                    print("\n%s is now on the stage."%(stage[-1].name))
-                except ValueError:
-                    print("A number, please.")
-                except IndexError:
-                    print("You don't have that many monsters!")
+        print("\nThis month's show is about %s."%(statname[stagetarget]))
+        flag = bayedit(stage,box,name="stage")
+        if flag:
+            break
 
 def runexhibit(stage, stagetarget):
     if len(stage) != 0:
@@ -388,7 +336,7 @@ def runexhibit(stage, stagetarget):
         showtotal += stage[0].stats[stagetarget]
         if len(stage)==2:
             showtotal += stage[1].stats[stagetarget]
-            bonus, null = bloodcheck(stage[0],stage[1])
+            bonus = bloodcheck(stage[0],stage[1])
             showtotal += (bonus*2)
             print("\n%s and %s put on an exhibition of %s."%(stage[0].name,stage[1].name,statname[stagetarget]))
             if bonus != 0:
@@ -396,7 +344,7 @@ def runexhibit(stage, stagetarget):
         else:
             print("\n%s puts on an exhibition of %s."%(stage[0].name,statname[stagetarget]))
     print("Your exhibition score is %i."%(showtotal))
-    tickets=ceil(showtotal/3)
+    tickets=int(showtotal/2)
     print("Ticket sales made you $%i."%(tickets))
     global money
     money+=tickets
@@ -414,15 +362,15 @@ save = False
 print("""Monster Breeding Game Demo
 by Nicholas Fletcher""")
 
-if isfile("save.db"):
+if isfile("box.db"):
     while True:
         print("\nThere is a save file.\nDo you want to [L]oad it or [S]tart a new name?")
         option = input("> ")
         if option in ("S","s"):
-            box.append(monster(randomgene(),[0,0],[],0,namegen(),age=2))
-            box.append(monster(randomgene(),[0,0],[],1,namegen(),age=2))
-            box.append(monster(randomgene(),[0,0],[],0,namegen(),age=2))
-            box.append(monster(randomgene(),[0,0],[],1,namegen(),age=2))
+            box.append(monster(randomgene(),0,namegen(),age=2))
+            box.append(monster(randomgene(),1,namegen(),age=2))
+            box.append(monster(randomgene(),0,namegen(),age=2))
+            box.append(monster(randomgene(),1,namegen(),age=2))
             break
         elif option in ("L","l"):
             with shelve.open('box') as savefile:
@@ -435,10 +383,15 @@ if isfile("save.db"):
                 count = int(savefile["limit"])
                 for i in range(0,count):
                     load=savefile[str(i)]
-                    null=monster(randomgene(),[0,0],[],0,"Null")
+                    null=monster(randomgene(),2,"Null")
                     null.saveload(load)
                     box.append(null)
             break
+else:
+    box.append(monster(randomgene(),0,namegen(),age=2))
+    box.append(monster(randomgene(),1,namegen(),age=2))
+    box.append(monster(randomgene(),0,namegen(),age=2))
+    box.append(monster(randomgene(),1,namegen(),age=2))
 
 while True:
     displaybox(box)
@@ -472,8 +425,10 @@ Select your action:
                 box.append(bays[i].pop(0))
             if baby != None:
                     box.append(baby)
-        box+=stage
-        stage=[]
+        if len(stage) != 0:
+            runexhibit(stage,stagetarget)
+            box+=stage
+            stage=[]
         for i in range(0,len(box)):
             box[i].passtime()
         stagetarget = randint(0,3)
